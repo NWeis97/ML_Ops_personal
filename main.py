8fb5ce05cf3cb420442973cd7ef9cd9b8e338c02
NWeis97 <nicolai.weisbjerg@gmail.com>
2022-01-04 10:11:28 +0100

    Added progression bar to training

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ class TrainOREvaluate(object):
                 
             else:
                 train_losses.append(running_loss/len(train_set))
-                print('epoch: ',str(e))
+                print('epoch: ',str(e), '/', str(epochs))
                 print('training_loss: ',str(train_losses[e]))
                 print('')
         #pdb.set_trace()
